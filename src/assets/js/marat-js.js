05c41d8954563ac1a619e1194b874b61c9532508
Marat Minchik <devminrat@gmail.com>
2023-02-07 17:47:36 +0300

    settings-common

--- src/assets/js/marat-js.js
+++ src/assets/js/marat-js.js
@@ -1,19 +1,47 @@
 const lkSertifRows = document.querySelectorAll(".lk__table_row:not(.head)");
 
-lkSertifRows.forEach((el) => {
-  let ckeckbox = el.querySelector("input");
+if (lkSertifRows.length) {
+  lkSertifRows.forEach((el) => {
+    let ckeckbox = el.querySelector("input");
 
-  if (ckeckbox.checked) {
-    el.classList.add("selected");
-  } else {
-    el.classList.remove("selected");
-  }
-
-  ckeckbox.addEventListener("change", function () {
-    if (this.checked) {
+    if (ckeckbox.checked) {
       el.classList.add("selected");
     } else {
       el.classList.remove("selected");
     }
+
+    ckeckbox.addEventListener("change", function () {
+      if (this.checked) {
+        el.classList.add("selected");
+      } else {
+        el.classList.remove("selected");
+      }
+    });
   });
-});
+}
+
+const enableDateBtns = document.querySelectorAll(".enable-date");
+
+if (enableDateBtns.length) {
+  enableDateBtns.forEach((el) => {
+    let input = el.querySelector("input");
+    let label = el.querySelector("label");
+    let fields = el.parentElement.querySelector(".lkset-item__fields");
+
+    if (el.checked) {
+      fields.classList.add("disabled");
+    } else {
+      fields.classList.remove("disabled");
+    }
+
+    input.addEventListener("change", function () {
+      if (this.checked) {
+        label.innerText = "Включить";
+        fields.classList.add("disabled");
+      } else {
+        label.innerText = "Выключить";
+        fields.classList.remove("disabled");
+      }
+    });
+  });
+}
