0b3d85710020757feeaee8ff25e1b0e34085e67a
Marat Minchik <devminrat@gmail.com>
2023-02-07 13:40:30 +0300

    custom-input

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,17 +2,14 @@ let openModal;
 let closeModal;
 
 document.addEventListener("DOMContentLoaded", () => {
-  //= marat-js.js
+//= marat-js.js
 
   let height = window.innerHeight;
   let width = window.innerWidth;
   let header = document.querySelector(".header");
-  document.documentElement.style.setProperty("--h", height + "px");
-  document.documentElement.style.setProperty("--w", width + "px");
-  document.documentElement.style.setProperty(
-    "--headerH",
-    header.getBoundingClientRect().height + "px"
-  );
+  document.documentElement.style.setProperty('--h', height + "px");
+  document.documentElement.style.setProperty('--w', width + "px");
+  document.documentElement.style.setProperty('--headerH', header.getBoundingClientRect().height + "px");
 
   //el.style.setProperty("--r", right + "px");
   //scrollLock.enablePageScroll(openedModal); отключить
@@ -28,40 +25,30 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   let topBtn = document.querySelectorAll(".btn-top");
-  let velocity = 0.1;
+  let velocity = .1;
   let pos = 0,
     topOffset = 0;
   let elemTop;
   let start;
   if (topBtn.length > 0) {
-    topBtn.forEach((btn) => {
+    topBtn.forEach(btn => {
       btn.onclick = (e) => {
         e.preventDefault();
         let winYOffset = window.pageYOffset,
           hash = "#top";
-        (elemTop =
-          document.querySelector(hash).getBoundingClientRect().top - topOffset),
-          (start = null);
+        elemTop = document.querySelector(hash).getBoundingClientRect().top - topOffset,
+          start = null;
         requestAnimationFrame(step);
         function step(time) {
           if (start === null) start = time;
           let progress = time - start,
-            r =
-              elemTop < 0
-                ? Math.max(
-                    winYOffset - progress / velocity,
-                    winYOffset + elemTop
-                  )
-                : Math.min(
-                    winYOffset + progress / velocity,
-                    winYOffset + elemTop
-                  );
+            r = (elemTop < 0 ? Math.max(winYOffset - progress / velocity, winYOffset + elemTop) : Math.min(winYOffset + progress / velocity, winYOffset + elemTop));
           window.scrollTo(0, r);
           if (r != winYOffset + elemTop) {
-            requestAnimationFrame(step);
+            requestAnimationFrame(step)
           } else return;
         }
-      };
+      }
     });
   }
 
@@ -84,7 +71,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let selectorTitle = document.querySelectorAll(".selector__title");
   if (selectorTitle.length > 0) {
-    selectorTitle.forEach((title) => {
+    selectorTitle.forEach(title => {
       title.onclick = (e) => {
         e.preventDefault();
         title.parentNode.classList.toggle("active");
@@ -94,13 +81,13 @@ document.addEventListener("DOMContentLoaded", () => {
         } else {
           panel.style.maxHeight = panel.scrollHeight + "px";
         }
-      };
+      }
     });
   }
 
   let selectorItems = document.querySelectorAll(".selector__item");
   if (selectorItems.length > 0) {
-    selectorItems.forEach((item) => {
+    selectorItems.forEach(item => {
       let parent = item.closest(".selector");
       let block = item.parentNode;
       let title = parent.querySelector(".selector__title span");
@@ -119,13 +106,13 @@ document.addEventListener("DOMContentLoaded", () => {
           block.style.maxHeight = null;
           parent.classList.remove("active");
         }
-      };
+      }
     });
   }
 
   let asideItems = document.querySelectorAll(".lk__aside_item");
   if (asideItems.length > 0) {
-    asideItems.forEach((item) => {
+    asideItems.forEach(item => {
       item.onclick = (e) => {
         e.preventDefault();
         if (!item.classList.contains("active")) {
@@ -135,7 +122,7 @@ document.addEventListener("DOMContentLoaded", () => {
           }
           item.classList.add("active");
         }
-      };
+      }
     });
   }
 
@@ -147,16 +134,9 @@ document.addEventListener("DOMContentLoaded", () => {
       this.btn = this.wrapper.querySelector(".select-btn");
       this.btnValue = this.btn.querySelector(".select-btn__value");
       this.container = this.wrapper.querySelector(".select-container");
-      this.isOpen =
-        this.wrapper.getAttribute("data-open") !== null ? true : false;
+      this.isOpen = this.wrapper.getAttribute("data-open") !== null ? true : false;
 
-      if (
-        this.wrapper &&
-        this.input &&
-        this.btn &&
-        this.btnValue &&
-        this.container
-      ) {
+      if (this.wrapper && this.input && this.btn && this.btnValue && this.container) {
         this.init();
       }
     }
@@ -165,16 +145,16 @@ document.addEventListener("DOMContentLoaded", () => {
       this.btnValue.innerText = this.input.placeholder;
       this.btnValue.classList.add("_placeholder");
 
-      this.maxHeight = (this.container.offsetHeight * 2) / 10 + "rem";
+      this.maxHeight = this.container.offsetHeight * 2 / 10 + "rem";
       this.btn.addEventListener("click", this.handleClick.bind(this));
 
-      [...this.container.children].forEach((item) => {
+      [...this.container.children].forEach(item => {
         item.addEventListener("click", this.setValue.bind(this, item));
-      });
+      })
 
       this.isOpen ? this.open() : this.close();
 
-      this.wrapper.addEventListener("click", (e) => e.stopPropagation());
+      this.wrapper.addEventListener("click", (e) => e.stopPropagation())
       document.addEventListener("click", this.close.bind(this));
     }
 
@@ -202,9 +182,9 @@ document.addEventListener("DOMContentLoaded", () => {
       this.btnValue.classList.remove("_placeholder");
       this.close();
 
-      [...this.container.children].forEach((i) => {
+      [...this.container.children].forEach(i => {
         i.classList.remove("_active");
-      });
+      })
 
       item.classList.add("_active");
     }
@@ -212,9 +192,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const selects = document.querySelectorAll(".select");
 
-  selects.forEach((item) => new Select(item));
+  selects.forEach(item => new Select(item))
 
-  // CHECKBOX
+  // CHECKBOX 
   class Checkbox {
     constructor(label) {
       this.label = label;
@@ -238,5 +218,8 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   const checkboxItems = document.querySelectorAll(".checkbox");
-  checkboxItems.forEach((item) => new Checkbox(item));
+  checkboxItems.forEach(item => new Checkbox(item));
 });
+
+
+
